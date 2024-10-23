e6311a065efb0679b8719d56c40f1d5b2b811427
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-10-23 02:26:04 -0500

    feat: fetching data

--- PushupFront-end/src/components/TopField.jsx
+++ PushupFront-end/src/components/TopField.jsx
@@ -1,36 +1,92 @@
-import React, { useState, useEffect, useRef } from 'react';
-import background from '../../public/storage/images/background.png'
+import React, { useState, useEffect } from 'react';
+import background from '../../public/storage/images/background.png';
 
 const TopField = () => {
+  const [City, setCity] = useState("Leticia");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/current.json?key=cac8c85072cf4893a3952959242210&q=${City}&lang=es`
+        );
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos del clima");
+        }
+        const data = await response.json();
+        setWeatherData(data); 
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeatherData(); 
+  }, [City]);
+
+  if (loading) return <p className='text-witheY'></p>;
+  if (error) return <p className='text-red-500'>{error}</p>;
+
   return (
-    <section
-      className='bg-lilaL h-[50vh] rounded-b-[50px] '
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center -113px',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <div className='flex justify-between'><p className='text-witheY pt-[40px] pl-[20px]'>Kharkiv, Ukraine</p>
-        <img src="../../public/storage/images/search_white.png" alt="Ícono" className='mr-2 w-5 h-5 mt-[45px] mr-[30px]' />
-      </div>
-      <div className='flex'><p className='text-witheY text-8xl font-regular pt-[70px] pl-[20px] w-[100vh]'> 3° </p>
-        <p className='text-witheY absolute pt-[135px] left-[80px] text-sm'>Feels like -2°</p>
-
-        <div className='flex-col'>
-          <img src="../../public/storage/images/Group.png" alt="Ícono" className='mr-2 w-25 h-22 mt-[20px] mr-[2px]' />
-          <p className='text-witheY text-xl  pt-[4px] pr-[45px]'>Cloudy</p></div>
-      </div>
-
-      <div className="flex justify-between items-end p-[25px] pt-[45px] text-sm"><p className='text-witheY'>January 18,16:14</p>
-        <div><p className='text-witheY'>Day 3°</p>
-          <p className='text-witheY'>Night -1°</p>
+    weatherData && (
+      <section
+        className='bg-lilaL h-[50vh] rounded-b-[50px]'
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center -113px',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className='flex justify-between'>
+          <p className='text-witheY pt-[40px] pl-[20px]'>
+            {weatherData.location.name}, {weatherData.location.region}
+          </p>
+          <img
+            src='../../public/storage/images/search_white.png'
+            alt='Ícono'
+            className='mr-2 w-5 h-5 mt-[45px] mr-[30px]'
+          />
         </div>
-      </div>
-    </section>
-  )
-}
 
+        <div className='flex'>
+          <p className='text-witheY text-6xl font-regular pt-[45px] pl-[20px] w-[100vh]'>
+            {weatherData.current.temp_c}°
+          </p>
+          <p className='text-witheY absolute pt-[135px] left-[55px] text-sm'>
+           Feels like {weatherData.current.feelslike_c}°
+          </p>
+
+          <div className='flex-col'>
+          <img
+  src={`http:${weatherData.current.condition.icon}`}
+  alt='Ícono'
+className='w-32 h-24 mt-[15px] mr-[70px]'
+/>
+
+<p className='text-witheY text-sm pt-[4px] pr-[45px]'>
+  {weatherData.current.condition.text}
+</p>
+
+          </div>
+        </div>
+
+        <div className='flex justify-between items-end p-[25px] pt-[70px] text-sm'>
+          <p className='text-witheY  text-sm'>{weatherData.location.localtime}</p>
+          <div>
+          <p className='text-witheY text-sm'>IsDay{weatherData.current.is_day}°</p>
+
+            
+          </div>
+        </div>
+      </section>
+    )
+  );
+};
 
 export default TopField;
