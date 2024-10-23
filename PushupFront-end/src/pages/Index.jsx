54cbdc14f05b4aba651d0c613efb25317e3cfa29
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-10-22 19:57:01 -0500

    feat: :construction: Many other advances have been done

---
+++ PushupFront-end/src/pages/Index.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const IndexPage = ()=>{
+
+    return(
+        <>
+        <h1 className='text-purple'>Let's Go!</h1>
+        </>
+
+    )
+
+}
+
+export default IndexPage;
